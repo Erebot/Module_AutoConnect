72bef72fc61dbae039c48d4475ba7cfbe7d38b8b
François Poirotte <clicky@erebot.net>
2012-06-23 17:46:03 +0200

    Update build env. & packaging.
    Prepare the module for the big migration to composer (to handle phar deps).

--- stub.php
+++ stub.php
@@ -1,52 +1,56 @@
-#!/usr/bin/env php
 <?php
-/**
- * If your package does special stuff in phar format, use this file.  Remove if
- * no phar format is ever generated
- * More information: http://pear.php.net/manual/en/pyrus.commands.package.php#pyrus.commands.package.stub
- */
+/*
+    This file is part of Erebot.
+
+    Erebot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Erebot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 if (version_compare(phpversion(), '5.3.1', '<')) {
     if (substr(phpversion(), 0, 5) != '5.3.1') {
         // this small hack is because of running RCs of 5.3.1
-        echo "Erebot_Module_AutoConnect requires PHP 5.3.1 or newer.
-";
-        exit -1;
+        echo "@PACKAGE_NAME@ requires PHP 5.3.1 or newer." . PHP_EOL;
+        exit(1);
     }
 }
 foreach (array('phar', 'spl', 'pcre', 'simplexml') as $ext) {
     if (!extension_loaded($ext)) {
-        echo 'Extension ', $ext, " is required
-";
-        exit -1;
+        echo "Extension $ext is required." . PHP_EOL;
+        exit(1);
     }
 }
 try {
     Phar::mapPhar();
 } catch (Exception $e) {
-    echo "Cannot process Erebot_Module_AutoConnect phar:
-";
-    echo $e->getMessage(), "
-";
-    exit -1;
+    echo "Cannot process @PACKAGE_NAME@ phar:" . PHP_EOL;
+    echo $e->getMessage() . PHP_EOL;
+    exit(1);
 }
-function Erebot_Module_AutoConnect_autoload($class)
-{
-    $class = str_replace(array('_', '\\'), '/', $class);
-    if (file_exists('phar://' . __FILE__ . '/Erebot_Module_AutoConnect-0.1.0/php/' . $class . '.php')) {
-        include 'phar://' . __FILE__ . '/Erebot_Module_AutoConnect-0.1.0/php/' . $class . '.php';
-    }
-}
-spl_autoload_register("Erebot_Module_AutoConnect_autoload");
-$phar = new Phar(__FILE__);
-$sig = $phar->getSignature();
-define('Erebot_Module_AutoConnect_SIG', $sig['hash']);
-define('Erebot_Module_AutoConnect_SIGTYPE', $sig['hash_type']);
 
-// your package-specific stuff here, for instance, here is what Pyrus does:
+// Return metadata about this package.
+return array(
+    'pear.erebot.net/@PACKAGE_NAME@' => array(
+        'version' => '@PACKAGE_VERSION@',
+        'path' =>
+            "phar://" . __FILE__ .
+            DIRECTORY_SEPARATOR . "@PACKAGE_NAME@-@PACKAGE_VERSION@" .
+            DIRECTORY_SEPARATOR . "php",
+        'requires' => array(
+            'php >= 5.2.2',
+            'virt-Erebot_API = 0.2.*',
+            'pear.erebot.net/Erebot',
+        ),
+    ),
+);
 
-/**
- * $frontend = new \PEAR2\Pyrus\ScriptFrontend\Commands;
- * @array_shift($_SERVER['argv']);
- * $frontend->run($_SERVER['argv']);
- */
 __HALT_COMPILER();
